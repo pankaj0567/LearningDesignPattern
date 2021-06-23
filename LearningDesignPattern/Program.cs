9a34d12d257ee2e8b0cd64d35c4226f90dac9ba4
pankaj <aiotechnicalpankaj@gmail.com>
2021-06-23 11:56:34 +0530

    Add project files.

---
+++ LearningDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
